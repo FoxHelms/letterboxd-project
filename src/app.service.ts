cb01fcb1f352e0d3f493002a208482b1ae78b94c
Fox Helms <foxhelms@gmail.com>
2024-04-14 17:45:31 -0400

    paginating

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,30 +9,34 @@ export class AppService {
   }
 
   async getAllFilms(): Promise<Film[]> {
-    const resp = await fetch(
-      'https://letterboxd.com/hershwin/list/all-the-movies/',
-    );
-    let i: number;
     const filmArray: Film[] = [];
-    const respBody = await resp.text();
-    const parsedPage = parse(respBody);
-    const filmsList = parsedPage.querySelectorAll(
-      '[class="poster-container numbered-list-item"]',
-    );
-    for (i = 0; i < filmsList.length; i++) {
-      const rawFilmString = filmsList.at(i).innerHTML;
-      const filmName = rawFilmString.match(
-        new RegExp('alt="' + '(.*)' + '"> <span class="frame">'),
+    for (let j = 1; j <= 349; j++) {
+      console.log(`Scraping page ${j}/349`);
+      const resp = await fetch(
+        `https://letterboxd.com/hershwin/list/all-the-movies/page/${j}/`,
       );
-      const filmId = rawFilmString.match(
-        new RegExp('data-film-id="' + '(.*)' + '" data-film-slug'),
+      let i: number;
+      const respBody = await resp.text();
+      const parsedPage = parse(respBody);
+      const filmsList = parsedPage.querySelectorAll(
+        '[class="poster-container numbered-list-item"]',
       );
-      filmArray.push({
-        name: filmName.at(1),
-        letterboxdId: filmId.at(1),
-        id: '',
-      });
+      for (i = 0; i < filmsList.length; i++) {
+        const rawFilmString = filmsList.at(i).innerHTML;
+        const filmName = rawFilmString.match(
+          new RegExp('alt="' + '(.*)' + '"> <span class="frame">'),
+        );
+        const filmId = rawFilmString.match(
+          new RegExp('data-film-id="' + '(.*)' + '" data-film-slug'),
+        );
+        filmArray.push({
+          name: filmName.at(1),
+          letterboxdId: filmId.at(1),
+          id: '',
+        });
+      }
     }
+
     return filmArray;
   }
 }
