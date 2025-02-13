1d77a1293b67fb4c6c94a6bfeee930f3782d3b87
FoxHelms <113215990+FoxHelms@users.noreply.github.com>
2025-02-12 23:00:57 -0500

    fix: Make film scraping correctly parse film names (#4)
    * paginating
    
    * getting name and id from letterboxd
    
    * remove redundant code
    
    * feat: Dynamically get max page in scrape all films
    
    ---------
    
    Co-authored-by: Ricky Pan <ricky990905@gmail.com>

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { parse } from 'node-html-parser';
+import { parse, HTMLElement } from 'node-html-parser';
 import { Film } from './films/film.entity';
 import { FilmService } from './films/film.service';
 
@@ -13,31 +13,43 @@ export class AppService {
     );
     let i: number;
     const filmArray: Film[] = [];
-    const respBody = await resp.text();
-    const parsedPage = parse(respBody);
-    const filmsList = parsedPage.querySelectorAll(
-      '[class="poster-container numbered-list-item"]',
-    );
-    for (i = 0; i < filmsList.length; i++) {
-      const rawFilmString = filmsList.at(i).innerHTML;
-      const filmName = rawFilmString.match(
-        new RegExp('alt="' + '(.*)' + '"> <span class="frame">'),
+    const maxPage = this.getMaxPage(parse(await resp.text()))
+  
+    for (let j = 1; j <= maxPage; j++) {
+      console.log(`Scraping page ${j}/${maxPage}`);
+      const resp = await fetch(
+        `https://letterboxd.com/hershwin/list/all-the-movies/page/${j}/`,
       );
-      const filmId = rawFilmString.match(
-        new RegExp('data-film-id="' + '(.*)' + '" data-film-slug'),
+      let i: number;
+      const respBody = await resp.text();
+      const parsedPage = parse(respBody);
+      const filmsList = parsedPage.querySelectorAll(
+        '[class="poster-container numbered-list-item"]',
       );
+      // console.log('film list item: ', filmsList.at(1));
+      // console.log('film list item inner html: ', filmsList.at(1).innerHTML);
 
+      filmsList.forEach((film) => {
       const newFilm = new Film();
-      newFilm.name = filmName.at(1);
-      newFilm.letterboxdId = filmId.at(1);
+        newFilm.name = film
+          .getElementsByTagName('div')
+          .at(0)
+          .getAttribute('data-film-slug');
+        newFilm.letterboxdId = film
+          .getElementsByTagName('div')
+          .at(0)
+          .getAttribute('data-film-id');
 
       filmArray.push(newFilm);
+      });
     }
 
     if (save) {
       const filmPromises = new Array<Promise<Film>>();
       for (const film of filmArray) {
-        filmPromises.push(this.filmService.saveFilm(film.name, film.letterboxdId));
+        filmPromises.push(
+          this.filmService.saveFilm(film.name, film.letterboxdId),
+        );
       }
       const res = await Promise.all(filmPromises);
       console.log(res);
@@ -46,7 +58,19 @@ export class AppService {
     return filmArray;
   }
 
+  private getMaxPage(parsedHtml: HTMLElement): number {
+    const paginatePages = parsedHtml.querySelectorAll('.paginate-page');
+    if (paginatePages.length === 0) {
+      return 1;
+    }
+    
+    const lastPage = paginatePages[paginatePages.length - 1];
+    const pageNumber = lastPage.querySelector('a')?.textContent;
+    
+    return pageNumber ? parseInt(pageNumber, 10) : 1;
+  }
+
   getAllFilms() {
-    return this.filmService.getAllFilms()
+    return this.filmService.getAllFilms();
   }
 }
